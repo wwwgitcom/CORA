559014dc1eed04d83313467dcaa4407f6598102a
wwwgitcom <bolotier@gmail.com>
2011-06-26 12:52:04 +0800

    prepare for 4x4...

--- Cora/blocks/b_dot11n_add_pilot.h
+++ Cora/blocks/b_dot11n_add_pilot.h
@@ -44,3 +44,47 @@ DEFINE_BLOCK(b_dot11n_add_pilot_1v, 1, 0)
     return true;
   }
 };
+
+
+DEFINE_BLOCK(b_dot11n_4x4_add_pilot_1v, 1, 0)
+{
+  _local_(int, pilot_index, 0);
+  _local_(int, iss, 1);
+  _local_(complex16, bpsk_one, complex16(30339, 0));
+
+  dot11n_4x4_pilot _pilot_tracker;
+  int16  _pilots[4];
+
+  BLOCK_INIT
+  {
+    auto v = $["iss"];
+    if (!v.empty())
+    {
+      *iss = atoi(v.c_str());
+    }
+  }
+
+  BLOCK_RESET
+  {
+    *pilot_index = 0;
+  }
+
+  BLOCK_WORK
+  {
+    auto n = ninput(0);
+    if (n < 1) return false;
+
+    auto ip = _$<dot11n_tx_symbol>(0);
+
+    _pilot_tracker(*iss, *pilot_index, _pilots);
+
+    ip->subcarriers[128 - 21]  = *bpsk_one * _pilots[0];
+    ip->subcarriers[128 - 7]   = *bpsk_one * _pilots[1];
+    ip->subcarriers[7]         = *bpsk_one * _pilots[2];
+    ip->subcarriers[21]        = *bpsk_one * _pilots[3];
+
+    (*pilot_index)++;
+
+    return true;
+  }
+};
